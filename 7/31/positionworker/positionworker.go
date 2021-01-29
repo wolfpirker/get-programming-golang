6e29299f7fee8f82a07326a5205eaa90bdfbd7ac
wolfpirker <w_pirker@gmx.de>
2021-01-29 14:56:29 +0100

    lesson 31, additional exercises

--- 7/31/positionworker/positionworker.go
+++ 7/31/positionworker/positionworker.go
@@ -8,18 +8,20 @@ import (
 
 func main() {
 	go worker()
-	time.Sleep(5 * time.Second)
+	time.Sleep(10 * time.Second)
 }
 func worker() {
 	pos := image.Point{X: 10, Y: 10}
 	direction := image.Point{X: 1, Y: 2}
-	next := time.After(time.Second)
+	delay := time.Second
+	next := time.After(delay)
 	for {
 		select {
 		case <-next:
 			pos = pos.Add(direction)
+			delay += 500 * time.Millisecond
 			fmt.Println("current position is ", pos)
-			next = time.After(time.Second)
+			next = time.After(delay)
 		}
 	}
 }
--- 7/31/rover/rover.go
+++ 7/31/rover/rover.go
@@ -10,7 +10,11 @@ func main() {
 	r := NewRoverDriver()
 	time.Sleep(2 * time.Second)
 	r.Left()
+	time.Sleep(time.Second)
+	r.Stop()
 	time.Sleep(2 * time.Second)
+	r.Start()
+	time.Sleep(time.Second)
 	r.Right()
 	time.Sleep(3 * time.Second)
 }
@@ -34,6 +38,8 @@ type command int
 const (
 	right = command(0)
 	left  = command(1)
+	start = command(2)
+	stop  = command(3)
 )
 
 // drive is responsible for driving the rover. It
@@ -42,6 +48,7 @@ func (r *RoverDriver) drive() {
 	pos := image.Point{X: 0, Y: 0}
 	direction := image.Point{X: 1, Y: 0}
 	updateInterval := 250 * time.Millisecond
+	prev_cmnd := right
 	nextMove := time.After(updateInterval)
 	for {
 		select {
@@ -52,16 +59,32 @@ func (r *RoverDriver) drive() {
 					X: -direction.Y,
 					Y: direction.X,
 				}
+				prev_cmnd = right
 			case left:
 				direction = image.Point{
 					X: direction.Y,
 					Y: -direction.X,
 				}
+				prev_cmnd = left
+			case start:
+				prev_cmnd = start
+			case stop:
+				prev_cmnd = stop
 			}
-			log.Printf("new direction %v", direction)
+			if (prev_cmnd == left) || (prev_cmnd == right) {
+				log.Printf("new direction %v", direction)
+			} else if prev_cmnd == stop {
+				log.Printf("rover stopped")
+			} else if prev_cmnd == start {
+				log.Printf("rover started")
+			}
+
 		case <-nextMove:
 			pos = pos.Add(direction)
-			log.Printf("moved to %v", pos)
+			if (prev_cmnd == left) || (prev_cmnd == right) {
+				log.Printf("moved to %v", pos)
+			}
+
 			nextMove = time.After(updateInterval)
 		}
 	}
@@ -75,4 +98,12 @@ func (r *RoverDriver) Left() {
 // Right turns the rover right (90° clockwise).
 func (r *RoverDriver) Right() {
 	r.commandc <- right
+}
+
+func (r *RoverDriver) Start() {
+	r.commandc <- start
+}
+
+func (r *RoverDriver) Stop() {
+	r.commandc <- stop
 }
