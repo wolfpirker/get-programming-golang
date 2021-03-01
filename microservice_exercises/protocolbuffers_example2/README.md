14d98244709ed304178696759d84322000336d83
wolfpirker <w_pirker@gmx.de>
2021-03-01 21:56:13 +0100

    corrected readme, shell script, deleted duplicate files

--- microservice_exercises/protocolbuffers_example2/README.md
+++ microservice_exercises/protocolbuffers_example2/README.md
@@ -1,6 +1,6 @@
 # Protocol Buffers Example in Golang
 
-This is a companion repository for my [Protocol Buffers course](http://bit.ly/protocol-buffers-github)
+This is a companion repository of Stephane Maareks [Protocol Buffers course](http://bit.ly/protocol-buffers-github)
 
 [![course logo](https://i.imgur.com/8fFmWAV.png)](http://bit.ly/protocol-buffers-github)
 
--- microservice_exercises/protocolbuffers_example2/generate.sh
+++ microservice_exercises/protocolbuffers_example2/generate.sh
@@ -1,3 +1,3 @@
-protoc -I src/ --go_out=src/ src/simple/simple.proto
-protoc -I src/ --go_out=src/ src/enum_example/enum_example.proto
-protoc -I src/ --go_out=src/ src/complex/complex.proto
+protoc -I src/ --go_out=src/simple/ src/simple/simple.proto
+protoc -I src/ --go_out=src/enum_example/ src/enum_example/enum_example.proto
+protoc -I src/ --go_out=src/complex/ src/complex/complex.proto
